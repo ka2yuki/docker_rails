bcc42a1055b9b21d0594731b12a86a32dce57ec6
katsuyuki <mori1103k@gmail.com>
2020-12-15 11:24:33 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 # 1. Docker Install
 
-if type 'docker-machine' > /dev/null 2>&1; then
-  echo "Exist docker-machine cmd."
-else
-  echo "docker-machine INSTALL"
-  echo $OSTYPE
-  case ${OSTYPE} in
-    darwin*)
-      # mac
-      curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
-        chmod +x /usr/local/bin/docker-machine
-      ;;
-    linux*)
-      curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
-        chmod +x /tmp/docker-machine &&
-        sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
-      ;;
-  esac
-  docker-machine
-fi
+# if type 'docker-machine' > /dev/null 2>&1; then
+#   echo "Exist docker-machine cmd."
+# else
+#   echo "docker-machine INSTALL"
+#   echo $OSTYPE
+#   case ${OSTYPE} in
+#     darwin*)
+#       # mac
+#       curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
+#         chmod +x /usr/local/bin/docker-machine
+#       ;;
+#     linux*)
+#       curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
+#         chmod +x /tmp/docker-machine &&
+#         sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+#       ;;
+#   esac
+#   docker-machine
+# fi
 
 
 if type 'docker-compose' > /dev/null 2>&1; then
@@ -34,34 +34,32 @@ docker-compose run web rails new . --force --database=postgresql
 # DOCUMENT: https://docs.docker.jp/compose/rails.html
 
 git remote remove origin
-
-
 ls -l
 sudo chown -R $USER:$USER .
 docker-compose build
 
 
 #!/bin/bash
-cat << EOT > config/database.yml
-default: &default
-  adapter: postgresql
-  encoding: unicode
-  host: db
-  username: postgres
-  password:
-  pool: 5
+# cat << EOT > config/database.yml
+# default: &default
+#   adapter: postgresql
+#   encoding: unicode
+#   host: db
+#   username: postgres
+#   password:
+#   pool: 5
 
-development:
-  <<: *default
-  database: myapp_development
+# development:
+#   <<: *default
+#   database: myapp_development
 
 
-test:
-  <<: *default
-  database: myapp_test
-EOT
+# test:
+#   <<: *default
+#   database: myapp_test
+# EOT
 
 docker-compose config # check.
 docker-compose up
 docker-compose run web rake db:create
-docker-machine ip MACHINE_VM
+# docker-machine ip MACHINE_VM
