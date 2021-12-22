d529b05d8956f4eb6cb3bd72a7776054a6d65d31
ka2yuki <workmail714@gmail.com>
2021-12-22 14:17:17 +0900

    init

--- Dockerfile
+++ Dockerfile
@@ -1,64 +1,26 @@
-# FROM centos:7
-FROM ruby:2.5
-RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
-    && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
-    && apt-get update -qq \
-    && apt-get install -y nodejs yarn \
-    && mkdir /myapp
-# rubyとrailsのバージョンを指定
-#ENV ruby_ver="2.6.4"
-#ENV rails_ver="5.1.4"
+FROM oiax/rails6-deps:latest
 
-# 必要なパッケージをインストール
-#RUN yum -y update
-#RUN yum -y install epel-release
-#RUN yum -y install git make autoconf curl wget
-#RUN yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel sqlite-devel bzip2
-#RUN yum clean all
+ARG UID=1000
+ARG GID=1000
 
-# rubyとbundleをダウンロード
-#RUN git clone https://github.com/sstephenson/rbenv.git /usr/local/rbenv
-#RUN git clone https://github.com/sstephenson/ruby-build.git /usr/local/rbenv/plugins/ruby-build
+RUN mkdir /var/mail
+RUN groupadd -g $GID devel
+RUN useradd -u $UID -g devel -m devel
+RUN echo "devel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-# コマンドでrbenvが使えるように設定
-#RUN echo 'export RBENV_ROOT="/usr/local/rbenv"' >> /etc/profile.d/rbenv.sh
-#RUN echo 'export PATH="${RBENV_ROOT}/bin:${PATH}"' >> /etc/profile.d/rbenv.sh
-#RUN echo 'eval "$(rbenv init --no-rehash -)"' >> /etc/profile.d/rbenv.sh
+WORKDIR /tmp
+COPY init/Gemfile /tmp/Gemfile
+COPY init/Gemfile.lock /tmp/Gemfile.lock
+RUN bundle install
 
-# rubyとrailsをインストール
-# RUN source /etc/profile.d/rbenv.sh; rbenv install ${ruby_ver}; rbenv global ${ruby_ver}
-# RUN source /etc/profile.d/rbenv.sh; gem update --system; gem install --version ${rails_ver} --no-ri --no-rdoc rails; gem install bundle
+COPY ./apps /apps
 
-####################
-# offical code add #
-####################
-WORKDIR /myapp
-COPY Gemfile /myapp/Gemfile
-COPY Gemfile.lock /myapp/Gemfile.lock
-# RUN gem install bundler -v 1.17.3
-# RUN gem install mysql2 -v '0.5.2' --source 'https://rubygems.org/' -- --with-cppflags=-I/usr/local/opt/openssl/include --with-ldflags=-L/usr/local/opt/openssl/lib
+RUN apk add --no-cache openssl shared-mime-info
 
-# # avoid error below.
-# RUN bundle config --local build.mysql2 "--with-ldflags=-L/usr/local/opt/openssl/lib --with-cppflags=-I/usr/local/opt/openssl/include"
-# RUN bundle install --path=vendor/bundle
-# RUN bundle show | grep mysql2
-# # avoid error :=> Could not find gem 'mysql2 (~> 0.5)' in any of the gem sources listed in your Gemfile.
-# bundle version ERROR Avoid!!
+USER devel
 
-# RUN gem update --system
-RUN gem install bundler:2.2.1
-# RUN gem install rails:6.0.0
-# RUN rails webpacker:install
-RUN bundle _2.2.1_ install 
-#     && rails db:create \
-#     && rails db:migrate
-COPY . /myapp
+RUN openssl rand -hex 64 > /home/devel/.secret_key_base
+RUN echo $'export SECRET_KEY_BASE=$(cat /home/devel/.secret_key_base)' \
+  >> /home/devel/.bashrc
 
-# Add a script to be executed every time the container starts.
-COPY entrypoint.sh /usr/bin/
-RUN chmod +x /usr/bin/entrypoint.sh
-ENTRYPOINT ["entrypoint.sh"]
-EXPOSE 3000
-
-# Start the main process.
-CMD ["rails", "server", "-b", "0.0.0.0"]
+WORKDIR /apps
--- setup.sh
+++ setup.sh
@@ -1,125 +1,18 @@
-# 1. Docker Install
-
-# !! docker-machine command check. if When you use it excute below.
-# if type 'docker-machine' > /dev/null 2>&1; then
-#   echo "Exist docker-machine cmd."
-# else
-#   echo "docker-machine INSTALL"
-#   echo $OSTYPE
-#   case ${OSTYPE} in
-#     darwin*)
-#       # mac
-#       curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
-#         chmod +x /usr/local/bin/docker-machine
-#       ;;
-#     linux*)
-#       curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
-#         chmod +x /tmp/docker-machine &&
-#         sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
-#       ;;
-#   esac
-#   docker-machine
-# fi
-
-# 2. docker-compose command check. Use frequently base-command.
-if type 'docker-compose' > /dev/null 2>&1; then
-  echo "Exist docker-compose cmd."
-  sleep 2
-else
-  echo "Docker-Compose INSTALL"
-  sleep 2
-  sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-  docker-compose
-fi
-
-# gem update --system
-# bundler update
-# gem install bundler
-# gem install bundler -v 1.17.3
-# bundle update --bundler
-# echo "bundle update --bundler"
-# sleep 2
-
-# ============================
-# Avoid Sql error below.
-# bundle config --local build.mysql2 "--with-ldflags=-L/usr/local/opt/openssl/lib --with-cppflags=-I/usr/local/opt/openssl/include"
-# bundle install --path=vendor/bundle
-# bundle show | grep mysql2
-
-# patarn2 
-# gem install mysql2 -v '0.5.2' --source 'https://rubygems.org/' -- --with-cppflags=-I/usr/local/opt/openssl/include --with-ldflags=-L/usr/local/opt/openssl/lib
-# avoid error :=> Could not find gem 'mysql2 (~> 0.5)' in any of the gem sources listed in your Gemfile.
-# ============================
-
-# プロジェクトのビルド. 引数別.
-# docker-compose run web rails new . --force --database=mysql
-# docker-compose run web rails new . --force --no-deps --database=mysql --skip-test --webpacker
-docker-compose run web rails new . --force --database=mysql --skip-test --webpacker
-# web: docker-compose.yml SERVICE Name.
-# rails new: rubyOnRails basic CLI command.
-# --webpacker: with webpacker install. Rails6以降は webpack のインストールも実行
-# --force: ??
-ls -l
-# ファイル一覧が表示されている: ls -l を機能させるポイントは、 web or ruby の image 削除する。
-# further more DOCUMENT: https://docs.docker.jp/compose/rails.html
-
-# git remote remove origin
-sudo chown -R $USER:$USER .
-echo ==========================
-echo "🚀 docker-compose build"
-echo ==========================
-sleep 2
-docker-compose build
-# docker build -t my_project/rails .
-
 #!/bin/bash
-# cat << EOT > config/database.yml
-# default: &default
-#   adapter: postgresql
-#   encoding: unicode
-#   host: db
-#   username: postgres
-#   password:
-#   pool: 5
+set -eu
 
-# development:
-#   <<: *default
-#   database: myapp_development
+docker pull oiax/rails6-deps:latest
 
-# test:
-#   <<: *default
-#   database: myapp_test
-# EOT
+BUILD_CMD="docker-compose build --no-cache"
 
-#!/bin/bash
-cat << EOT > config/database.yml
-default: &default
-  adapter: mysql2
-  encoding: utf8mb4
-  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
-  username: <%= ENV.fetch("MYSQL_USERNAME", "root") %>
-  password: <%= ENV.fetch("MYSQL_PASSWORD", "password") %>
-  host: <%= ENV.fetch("MYSQL_HOST", "db") %>
-
-development:
-  <<: *default
-  database: myapp_development
-
-test:
-  <<: *default
-  database: myapp_test
-
-production:
-  <<: *default
-  database: myapp_production
-  username: myapp
-  password: <%= ENV['MYAPP_DATABASE_PASSWORD'] %>
-EOT
-# 📌 user, pass, host の 一致は MUST!! 
-# (database-compose.yml == database.yml)
-
-docker-compose run --rm web rails db:create
-# docker-compose run web bin/rake db:create db:migrate db:seed
-docker-compose up 
-# docker-machine ip MACHINE_VM
+case "$OSTYPE" in
+  darwin*)
+    $BUILD_CMD web
+    ;;
+  linux*)
+    $BUILD_CMD --build-arg UID=$(id -u) --build-arg GID=$(id -g) web
+    ;;
+  *)
+    echo "Unknown OS Type: $OSTYPE"
+    ;;
+esac
